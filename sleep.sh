5737fc79c054e13b8e4ee3d481efde2af1a602c8
Motiejus Jakštys <motiejus@uber.com>
2019-07-30 10:40:26 +0300

    remove sleep.sh
    will add it to monorepo

--- sleep.sh
+++
@@ -1,6 +0,0 @@
-# lock your screen by whoever
-_motiejus() {
-    local sleeptime=$((cat $HOME/.motiejus 2>/dev/null || echo -n 0; echo +0.1) | bc)
-    echo -n $sleeptime | tee $HOME/.motiejus
-}
-sleep $(_motiejus)
